926505eed191c9390989396094717a38bc0c254f
Akazaki <akazaki0810@gmail.com>
2021-01-05 21:53:34 +0900

    with_optionsでまとめて記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,10 +10,13 @@ class User < ApplicationRecord
   with_options presence: true do
     validates :nickname
     validates :birthday
-    validates :firstname, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "は全角で入力してください。"}
-    validates :lastname, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "は全角で入力してください。"}
-    validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: "は全角カタカナで入力して下さい。"}
-    validates :last_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: "は全角カタカナで入力して下さい。"}
     validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze, message: "には英字と数字の両方を含めて設定してください。"}
+    with_options format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "は全角で入力してください。"} do
+    validates :firstname
+    validates :last_name
+    end
+    with_options format: { with: /\A[ァ-ヶー－]+\z/, message: "は全角カタカナで入力して下さい。"} do
+    validates :first_name_kana
+    validates :last_name_kana
   end
 end
