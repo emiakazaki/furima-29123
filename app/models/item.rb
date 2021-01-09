9f5813acb427d12a8d718e1c24d81bfb5819c76e
Akazaki <akazaki0810@gmail.com>
2021-01-09 15:44:54 +0900

    ActiveHashを導入する

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,4 +4,9 @@ class Item < ApplicationRecord
   validates :image, presence: true
   has_one :order
   belongs_to :user
+  belongs_to_active_hash :fee_id 
+| belongs_to_active_hash :state_id
+| belongs_to_active_hash :category_id
+| belongs_to_active_hash :prefecture_id
+| belongs_to_active_hash :time_id   
 end
---
+++ spec/factories/time_ids.rb
@@ -0,0 +1,5 @@
+FactoryBot.define do
+  factory :time_id do
+    
+  end
+end
