01d3c9b456476cde9d33a6839cc78986f0927a46
Akazaki <akazaki0810@gmail.com>
2021-01-11 15:14:25 +0900

    商品出品ページを表示できるようにする

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,14 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!,only: [:get, :new, :update, :create, :destroy, :edit]
+
+  def new
+    @item = Item.new
+  end
+
   private
 
   def items_params
     params.require(:item).permit(:content, :image)
   end
+
 end
